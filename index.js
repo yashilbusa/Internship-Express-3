dc49e96cf7f38d585905b848c3d671f017a73a8b
yashilbusa <yashilpatel1914@gmail.com>
2025-02-20 11:37:55 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import mongoose from 'mongoose';
+
+const app = express()
+const port = 4004
+
+try {
+    await mongoose.connect(process.env.url);
+    console.log('Connected to MongoDB');
+    
+    app.listen(port, () => {
+      console.log(`Server Running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+}
